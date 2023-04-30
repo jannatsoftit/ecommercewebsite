b1ebaf7e2b7e149db4a544f24e7c25f2856ed84b
jannatsoftit <jannatlamitlogiko@gmail.com>
2023-04-30 16:33:27 +0600

    fourth update

--- app/Http/Livewire/ShopComponent.php
+++ app/Http/Livewire/ShopComponent.php
@@ -12,6 +12,7 @@ class ShopComponent extends Component
 {
     use WithPagination;
     public $pagesize = 12;
+    public $orderBy = 'Default Sorting';
 
     public function store($product_id,$product_name,$product_price){
         Cart::add($product_id,$product_name,1,$product_price)->associate('\App\Models\Product');
@@ -19,13 +20,34 @@ class ShopComponent extends Component
         return redirect('cart');
     }
 
-    public function changePageSize($size){
+    public function changePageSize($size)
+    {
         $this->pagesize = $size;
+    }
+
+    public function changeOrderBy($order)
+    {
+        $this->orderBy = $order;
     }
 
     public function render()
     {
-        $products = Product::paginate($this->pagesize);
+        if($this->orderBy == 'Price: Low to High')
+        {
+            $products = Product::orderBy('regular_price','ASC')->paginate($this->pagesize);
+        }
+        elseif($this->orderBy == 'Price: High to Low')
+        {
+            $products = Product::orderBy('regular_price','DESC')->paginate($this->pagesize);
+        }
+        elseif($this->orderBy == 'Sort By Newness')
+        {
+            $products = Product::orderBy('created_at','DESC')->paginate($this->pagesize);
+        }else{
+
+            $products = Product::paginate($this->pagesize);
+        }
+
         return view('livewire.shop-component',['products' => $products]);
     }
 }
